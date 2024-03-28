b3827ee25f42c2cd489be63741a1aef22df14938
Badwolfpup <Adam.Folke@hudiksvall.se>
2024-03-28 11:22:15 +0100

    Add project files.

---
+++ Bildspel/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bildspel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PreviousImage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NextImage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void StartSlideshow_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddImage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RemoveImage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
